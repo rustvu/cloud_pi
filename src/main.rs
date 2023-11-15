eea7c131e4fd1e8337d4be76b8bd724e81c764f5
Peter Volgyesi <peter.volgyesi@gmail.com>
2023-11-15 09:11:22 -0600

    add static web page

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use rand::random;
 use rocket::http::CookieJar;
 use rocket::http::Status;
 use rocket::serde::json::Json;
-use serde::{Serialize, Deserialize};
+use rocket::fs::FileServer;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 struct MonteCarloResult {
@@ -18,10 +19,11 @@ struct MonteCarloResult {
 #[get("/monte_carlo?<n>")]
 fn monte_carlo(cookies: &CookieJar, n: Option<u64>) -> Json<MonteCarloResult> {
     let total = cookies
-            .get("n")
-            .map(|c| c.value())
-            .map(|v| v.parse().ok())
-            .unwrap_or(n).unwrap_or(100_000);
+        .get("n")
+        .map(|c| c.value())
+        .map(|v| v.parse().ok())
+        .unwrap_or(n)
+        .unwrap_or(100_000);
 
     let mut inside = 0;
     for _ in 0..total {
@@ -51,26 +53,27 @@ fn defaults(cookies: &CookieJar, n: Option<u64>) -> Status {
 
 #[launch]
 fn app() -> _ {
-    rocket::build().mount("/pi", routes![monte_carlo, defaults])
+    rocket::build()
+        .mount("/pi", routes![monte_carlo, defaults])
+        .mount("/", FileServer::from("static"))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rocket::local::blocking::Client;
-    use rocket::http::Status;
     use approx::assert_relative_eq;
-
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
 
     #[test]
     fn test_monte_carlo() {
         let n = 1000;
         let client = Client::tracked(app()).unwrap();
         let response = client.get(format!("/pi/monte_carlo?n={n}")).dispatch();
-        
+
         assert_eq!(response.status(), Status::Ok);
 
-        let result: MonteCarloResult =  response.into_json().unwrap();
+        let result: MonteCarloResult = response.into_json().unwrap();
         assert_eq!(result.total, n);
         assert_relative_eq!(result.pi, std::f64::consts::PI, epsilon = 1.0);
     }
